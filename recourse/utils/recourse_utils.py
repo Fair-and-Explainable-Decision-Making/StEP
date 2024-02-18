8c52abd3331d24f7850d33eaaef0d59c05932865
nperello <nperello@live.com>
2024-02-18 01:11:48 -0500

    recourse util fix and limit for FACE sample size to prevent memory errors

--- recourse/utils/recourse_utils.py
+++ recourse/utils/recourse_utils.py
@@ -17,11 +17,11 @@ def get_recourse_interface_by_name(recourse_name: str, model_interface: ModelInt
                             confidence_threshold=kwargs['confidence_threshold'],random_seed=kwargs['random seed'])
     elif recourse_name == "FACE":
         return FACERecourse(model_interface, data_interface, k_directions=kwargs['k_directions'], distance_threshold=kwargs['direction_threshold'], 
-                            confidence_threshold=kwargs['confidence_threshold'],weight_bias=kwargs['weight_bias'])
+                            confidence_threshold=kwargs['confidence_threshold'],weight_bias=kwargs['weight_bias'],graph_sample_size=20000)
     else:
         raise Exception("Invalid recourse choice")
 
-def constrain_one_hot_cat_featutes(poi: pd.DataFrame, data_interface: DataInterface):
+def constrain_one_hot_cat_features(poi: pd.DataFrame, data_interface: DataInterface):
     for feat_name, one_hot_feat_names in data_interface.get_encoded_categorical_feats().items():
         one_hot_feats_constrained = np.zeros_like(
             poi[one_hot_feat_names].values)
